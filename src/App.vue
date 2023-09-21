a6d797d9f91864d2f761ffecf71bf8b74efcf225
lion199908188 <lion852147@gmail.com>
2023-09-21 13:41:42 +0800

    開始前端框架

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,14 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <v-toolbar>
+      <v-toolbar-title>主頁頂端測試</v-toolbar-title>
+      &emsp;
+      <v-divider vertical></v-divider>
+      &emsp;
+      <router-link to="/">主頁</router-link>
+      &emsp;
+      <router-link to="/about">測試1</router-link>
+    </v-toolbar>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    {{ tt }}
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  components: {
+   
+  },
+  data() {
+    return {
+      tt : 'test'
+    }
+  },
+  methods:{
+  
+  },
+}
+</script>
